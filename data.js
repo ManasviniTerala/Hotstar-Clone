74f5c2fc61573fc382a0d9bf5ca0d1e7f5fd4e6b
Manasvini Terala <tmanasvini244@gmail.com>
2024-09-07 10:46:37 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+        name: 'falcon and the winter soldier',
+        des: 'Following the events of Avengers: Endgame, Sam Wilson/Falcon and Bucky Barnes/Winter Soldier team up in a global adventure that tests their abilities -- and their patience.',
+        image: 'images/slider 2.PNG'
+    },
+    {
+        name: 'loki',
+        des: 'The mercurial villain Loki resumes his role as the God of Mischief in a new series that takes place after the events of Avengers: Endgame.',
+        image: 'images/slider 1.PNG'
+    },
+    {
+        name: 'wanda vision',
+        des: 'WandaVision is an American television miniseries created by Jac Schaeffer for the streaming service Disney+, based on Marvel Comics featuring the characters Wanda Maximoff / Scarlet Witch and Vision.',
+        image: 'images/slider 3.PNG'
+    },
+    {
+        name: 'raya and the last dragon',
+        des: 'Long ago, in the fantasy world of Kumandra, humans and dragons lived together in harmony. However, when sinister monsters known as the Druun threatened the land, the dragons sacrificed themselves to save humanity. ',
+        image: 'images/slider 4.PNG'
+    },
+    {
+        name: 'luca',
+        des: 'Set in a beautiful seaside town on the Italian Riviera, the original animated feature is a coming-of-age story about one young boy experiencing an unforgettable summer filled with gelato.',
+        image: 'images/slider 5.PNG'
+    }
+]
